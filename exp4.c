1729387cae9addedbd64343c0ecafab55dcef11a
apoca <zhaoang909@qq.com>
2024-04-08 07:23:04 +0000

    123

--- exp4.c
+++ exp4.c
@@ -1,8 +1,23 @@
 #include "stdio.h"
 
+void exp4_task1()
+{
+     // 输入两个正整数，求其中最大公约数和最小公倍数。
+     int a, b, n, m;
+     sscanf("32 24", "%d %d", &a, &b);
+     printf("a=%d b=%d\n", a, b);
+     m = a * b;
+     n = a % b;
+     while (n != 0)
+     {
+          a = b;
+          b = n;
+          n = a % b;
+     }
+     n = b;
+     printf("%d %d\n", n, m / n);
+}
 void main()
 {
-     int a, b;
-     sscanf("123,456", "%d,%d", &a, &b);
-     printf("a=%d b=%d\n", a, b);
+     exp4_task1();
 }
